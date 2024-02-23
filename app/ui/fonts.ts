4dd9bbb9886f19679aaaa995eb7bb641c7e5bc22
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-02-23 13:28:54 -0500

    Began adding database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Karla } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+const karla = Karla({ subsets: ['latin'] });
+const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+
+export { karla, lusitana };
